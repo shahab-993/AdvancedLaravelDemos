bded530acb6ef3d3104a65358e839e0e7f16400f
shahab-993 <ahmad.k.shahab@gmail.com>
2026-07-12 23:55:58 -0700

    second

--- app/Http/Controllers/RadioButtonDemoController.php
+++ app/Http/Controllers/RadioButtonDemoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\AllowanceTypes;
+use App\Models\EmployeesAllowances;
+use App\Models\PartTimeEmployees;
 use Illuminate\Http\Request;
 
 class RadioButtonDemoController extends Controller
@@ -14,6 +16,20 @@ class RadioButtonDemoController extends Controller
 
     }
     public function store(Request $request){
+        $employee=PartTimeEmployees::create([  
+            'EmployeeName'=>$request->EmployeeName,
+            'Salary'=>$request->Salary,
+            'AllowanceAmount'=> $request->AllowanceAmount,
+            'NetSalary'=> $request->NetSalary,
+
+        ]) ;
+
+        EmployeesAllowances::create([
+            'employee_id'=>$employee->id,
+            'allowacne_type_id'=> $request->AllowanceTypeID,
+            
+            ]);
+            return redirect()->route('workwithradiobuttons.create')->with('success','Employee added successfully!');
 
     } 
 }
--- database/migrations/2026_07_06_134443_create_employees_allowances_table.php
+++ database/migrations/2026_07_06_134443_create_employees_allowances_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees_allowances', function (Blueprint $table) {
-               $table->id();
+            $table->id();
+            
             
             $table->timestamps();
         });
