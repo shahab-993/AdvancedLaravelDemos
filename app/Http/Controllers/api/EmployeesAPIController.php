066e7f7ae4578de0b5bc982abce8f8f3b3830e48
shahab-993 <ahmad.k.shahab@gmail.com>
2026-09-17 19:37:55 +0430

    api2

--- app/Http/Controllers/api/EmployeesAPIController.php
+++ app/Http/Controllers/api/EmployeesAPIController.php
@@ -13,4 +13,38 @@ class EmployeesAPIController extends Controller
         return response()->json($employees);
         
     }
+    public function show($id){
+        $employee = Employee::with(['department','country'])->findOrFail($id);
+        return response()->json($employee);
+
+    }
+public function store(Request $request)
+{
+    $employee= Employee::create($request->all());
+    return response()->json([
+        'message'=>'Employee created successfully!','employee'=>$employee
+    ],201);
+}
+
+
+public function update(Request $request,$id){
+
+$employee = Employee::findOrFail($id);
+$employee->update($request->all());
+return response()->json([
+    'message'=>'Employee updated successfully!',
+    'employee'=>$employee
+],200);
+
+}
+
+public function destroy($id){
+    $employee =Employee::findOrFail($id);
+    $employee->delete();
+    return response()->json([
+        'message'=>'Employee deleted successfully!',
+       'employee'=> $employee
+    ]);
+
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
  
 Route::prefix('employees')->group(function(){
     Route::get('/',[EmployeesAPIController::class,'index']);
+    Route::get('/{id}',[EmployeesAPIController::class,'show']);
+    Route::post('/store',[EmployeesAPIController::class,'store']);
+    Route::put('/{id}',[EmployeesAPIController::class,'update']);
+    Route::delete('/{id}',[EmployeesAPIController::class,'destroy']);
 });
 Route::get('/departments',[DepartmentsAPIController::class,'index']);
 Route::get('/countries',[CountriesAPIController::class,'index']); 
